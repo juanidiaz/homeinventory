2dc244724ae8d5fc38dbd74d4a45b2c1bc97634e
Juan I Diaz <jdi@idiaz.ca>
2020-09-12 23:27:33 -0400

    Adding env variables

--- components/NavBar.js
+++ components/NavBar.js
@@ -23,15 +23,16 @@ export default function NavBar() {
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto" onClick={() => setExpanded(false)}>
               <Link href="/categories"><a className="nav-link">Categories</a></Link>
-              <Link href="/companies"><a className="nav-link">Companies</a></Link>
-              <Link href="/conditions"><a className="nav-link">Conditions</a></Link>
+              {/* <Link href="/companies"><a className="nav-link">Companies</a></Link> */}
+              {/* <Link href="/conditions"><a className="nav-link">Conditions</a></Link> */}
               <Link href="/contacts"><a className="nav-link">Contacts</a></Link>
-              <Link href="/contracts"><a className="nav-link">Contracts</a></Link>
-              <Link href="/items"><a className="nav-link">Items</a></Link>
-              <Link href="/locations"><a className="nav-link">Locations</a></Link>
-              <Link href="/policies"><a className="nav-link">Policies</a></Link>
-              <Link href="/rooms"><a className="nav-link">Rooms</a></Link>
-              <Link href="/subCategories"><a className="nav-link">Subcategories</a></Link>
+              <Link href="/info"><a className="nav-link">Info</a></Link>
+              {/* <Link href="/contracts"><a className="nav-link">Contracts</a></Link> */}
+              {/* <Link href="/items"><a className="nav-link">Items</a></Link> */}
+              {/* <Link href="/locations"><a className="nav-link">Locations</a></Link> */}
+              {/* <Link href="/policies"><a className="nav-link">Policies</a></Link> */}
+              {/* <Link href="/rooms"><a className="nav-link">Rooms</a></Link> */}
+              {/* <Link href="/subCategories"><a className="nav-link">Subcategories</a></Link> */}
             </Nav>
           </Navbar.Collapse>
         </Navbar>
