c6fd015b32650e4c160a8ecf225314c7b0269031
Juan I Diaz <jdi@idiaz.ca>
2020-09-21 15:37:27 -0400

    AWS S3 upload working without path info

--- src/pages/api/files.js
+++ src/pages/api/files.js
@@ -5,6 +5,7 @@ import Busboy from 'busboy';
 import uuid from 'node-uuid';
 const multer = require('multer');
 const multerS3 = require('multer-s3');
+const fs = require('fs');
 
 dbConnect();
 
@@ -62,101 +63,97 @@ export default async (req, res) => {
     case 'POST':
       try {
 
-        // console.log("UPLOADING FILE")
-        // s3upload(params);
-        // res.status(200).json({ success: true, message: "UPLOADING FILE" });
-
-        AWS.config.update({
-          accessKeyId: process.env.AWSAccessKeyId,
-          secretAccessKey: process.env.AWSSecretKey,
-          region: 'ca-central-1'
-        });
 
         const s3 = new AWS.S3();
+        console.log("UPLOADING FILE")
+        const bodyArray = req.body.split(/\r\n/)
+        const fileName = bodyArray[1].split('; ')[2].split('=')[1].slice(1, -1);
+        const fType = bodyArray[2].split('=')[1]
 
-        const fileFilter = (req, file, cb) => {
-          if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-            cb(null, true);
-          } else {
-            cb(new Error('Invalid file type, only JPEG and PNG is allowed!'), false);
-          }
-        }
+        console.log("=== FILENAME\n", fileName)
+        const fileContent = fs.readFileSync('/home/jdiaz/Downloads/' + fileName);
 
-        const upload = multer({
-          fileFilter,
-          storage: multerS3({
-            acl: 'public-read',
-            s3,
-            bucket: 'bwm-ng-dev',
-            metadata: function (req, file, cb) {
-              cb(null, { fieldName: 'TESTING_METADATA' });
-            },
-            key: function (req, file, cb) {
-              cb(null, Date.now().toString())
+        s3.createBucket({ Bucket: process.env.AWSfiles_BucketName, region: process.env.AWSfiles_BucketRegion }, function () {
+          var params = {
+            Bucket: process.env.AWSfiles_BucketName,
+            Key: fileName,
+            Body: fileContent,
+            // Body: req.body.split(/\r\n/)[4],
+            ACL: 'public-read',
+            ContentType: fType
+          };
+          s3.putObject(params, function (err, data) {
+            if (err) {
+              console.log(err);
+              res.status(403).json({ success: false, message: error.message });
+            } else {
+              console.log("Successfully uploaded data to " + process.env.AWSfiles_BucketName + "/" + fileName);
+              res.status(200).json({ data, fileName, bodyArray, body: req.body });
             }
-          })
+          });
         });
 
-        const singleUpload = upload.single('image');
-
-        singleUpload(req, res, function (err) {
-          if (err) {
-            return res.status(422).send({ errors: [{ title: 'Image Upload Error', detail: err.message }] });
-          }
-
-          return res.json({ 'imageUrl': req.file.location });
-        });
-
-
-        // const s3 = new AWS.S3();
-
-        // // app.post("/upload", (req, res) => {
+        //! = START ======================================================================== 
+        // app.post("/upload", (req, res) => {
         // let chunks = [], fname, ftype, fEncoding;
+        //   console.log("SHIT!", req.body);
         // let busboy = new Busboy({ headers: req.headers });
         // busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
         //   console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
-        //   fname = filename.replace(/ /g, "_");
-        //   ftype = mimetype;
-        //   fEncoding = encoding;
         //   file.on('data', function (data) {
-        //     // you will get chunks here will pull all chunk to an array and later concat it.
-        //     console.log(chunks.length);
-        //     chunks.push(data)
+        //     console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
         //   });
         //   file.on('end', function () {
-        //     console.log('File [' + filename + '] Finished');
+        //     console.log('File [' + fieldname + '] Finished');
         //   });
         // });
+        
+        //   busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
+        //     console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+        //     fname = filename.replace(/ /g, "_");
+        //     ftype = mimetype;
+        //     fEncoding = encoding;
+        //     file.on('data', function (data) {
+        //       // you will get chunks here will pull all chunk to an array and later concat it.
+        //       console.log(chunks.length);
+        //       chunks.push(data)
+        //     });
+        //     file.on('end', function () {
+        //       console.log('File [' + filename + '] Finished');
+        //     });
+        //   });
 
-        // busboy.on('finish', function () {
-        //   const userId = uuid.v4();
-        //   const params = {
-        //     Bucket: process.env.AWSBucketName_files, // your s3 bucket name
-        //     Key: `${userId}-${fname}`,
-        //     Body: Buffer.concat(chunks), // concatinating all chunks
-        //     ACL: 'public-read',
-        //     ContentEncoding: fEncoding, // optional
-        //     ContentType: ftype // required
-        //   }
-        //   // we are sending buffer data to s3.
-        //   s3.upload(params, (err, s3res) => {
-        //     if (err) {
-        //       if (err.statusCode) {
-        //         res.status(err.statusCode).json({ success: false, err })
-        //       } else {
-        //         res.status(400).send({ err, status: 'error' });
-        //       }
-        //     } else {
-        //       res.send({ data: s3res, status: 'success', msg: 'Image successfully uploaded.' });
+        //   busboy.on('finish', function () {
+        //     const userId = uuid.v4();
+        //     const params = {
+        //       Bucket: process.env.AWSBucketName_files, // your s3 bucket name
+        //       Key: `${userId}-${fname}`,
+        //       Body: Buffer.concat(chunks), // concatinating all chunks
+        //       ACL: 'public-read',
+        //       ContentEncoding: fEncoding, // optional
+        //       ContentType: ftype // required
         //     }
-        //   });
+        //     // we are sending buffer data to s3.
+        //     s3.upload(params, (err, s3res) => {
+        //       if (err) {
+        //         if (err.statusCode) {
+        //           res.status(err.statusCode).json({ success: false, err })
+        //         } else {
+        //           res.status(400).send({ err, status: 'error' });
+        //         }
+        //       } else {
+        //         res.send({ data: s3res, status: 'success', msg: 'Image successfully uploaded.' });
+        //       }
+        //     });
 
-        // });
-        // req.pipe(busboy);
-        // });
+        //   });
+        //   req.pipe(busboy);
+        // // });
+        //! = END ======================================================================== 
 
       } catch (error) {
-        res.status(400).json({ success: false, message: error.message });
+        console.log("ERROR!\n", error)
+        res.status(401).json({ success: false, message: error.message });
 
       }
       break;
