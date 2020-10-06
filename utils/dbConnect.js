3f26e1258b69737851a416dee0c70493438c4354
Juan I Diaz <jdi@idiaz.ca>
2020-10-05 23:57:21 -0400

    More trobleshooting

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -20,6 +20,7 @@ export default async function dbConnect() {
 
     const connectionString = "mongodb+srv://" + process.env.MONGO_USERNAME + ":" + process.env.MONGO_PASSWORD + "@" + process.env.MONGO_URL + "/" + process.env.MONGO_DB + "?retryWrites=true&w=majority"
 
+    console.log("--- SOURCE FROM --- ", process.env.SOURCE);
     console.log("--- ATTEMPING DB CONNECTION TO: ", connectionString);
 
     const db = await mongoose.connect(connectionString, mongooseOptions);
