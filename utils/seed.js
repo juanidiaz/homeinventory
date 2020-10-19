1ddf2b95bf9598369723b4f8538a8d318aa98c8c
Juan I Diaz <jdi@idiaz.ca>
2020-10-18 23:51:39 -0400

    Implemented lodash to include linked collection info

--- utils/seed.js
+++ utils/seed.js
@@ -1,6 +1,8 @@
 import faker from "faker";
 import SubCategory from "../src/models/SubCategory";
+import Category from "../src/models/Category";
 import dbConnect from "./dbConnect";
+import _ from "lodash";
 
 dbConnect();
 
@@ -24,7 +26,38 @@ export const seedSubCategory = async () => {
     subCategories.forEach((subCategory, index) => {
 
       SubCategory.create(subCategory).then(data => {
-        console.log("--", data);
+        console.log(`Created - subCategory #${index}`);
+      })
+
+    })
+
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+export const seedCategory = async () => {
+  try {
+    const loop = 5;
+    const categories = [];
+    const allSubCategories = await SubCategory.find();
+
+    for (let i = 0; i < loop; i++) {
+
+      categories.push(
+        new Category({
+          name: faker.name.findName(),
+          description: faker.lorem.paragraph(),
+          subCategories: [_.sample(allSubCategories)],
+          isActive: faker.random.boolean(),
+        })
+      )
+    }
+
+    categories.forEach((category, index) => {
+
+      Category.create(category).then(data => {
+        console.log(`Created - category #${index}`);
       })
 
     })
@@ -35,3 +68,4 @@ export const seedSubCategory = async () => {
 };
 
 seedSubCategory();
+seedCategory();
