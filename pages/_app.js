7edec941ea17170145bf03eabc1f7c71d5a61ba8
Juan Diaz <jdi@idiaz.ca>
2021-06-22 23:05:37 -0400

    Added HEAD component

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,22 @@
-import { Provider } from 'next-auth/client';
+import { Provider } from "next-auth/client";
+import Head from "next/head";
 
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import Layout from "../components/layout/layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider session={pageProps.session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+    <>
+      <Head>
+        <title>myInventory</title>
+      </Head>
+
+      <Provider session={pageProps.session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+    </>
   );
 }
 
