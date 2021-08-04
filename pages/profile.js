e34c2dbf7fc8547af767e005eb1535daeda53597
Juan Diaz <jdi@idiaz.ca>
2021-08-04 16:31:13 -0400

    Removing all consoles

--- pages/profile.js
+++ pages/profile.js
@@ -12,7 +12,6 @@ function ProfilePage(props) {
 
 export async function getServerSideProps(context) {
   const session = await getSession({ req: context.req });
-  console.log("---- getSession ----", session)
 
   if (!session) {
     return {
